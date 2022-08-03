aa27cd150a66d43569013b3b53d0de606a9307ab
ThiccPan <ihilmiderian@gmail.com>
2022-08-03 22:01:48 +0700

    add sidebar links

--- config/adminlte.php
+++ config/adminlte.php
@@ -277,53 +277,16 @@ return [
         ],
         ['header' => 'Foto'],
         [
-            'text' => 'profile',
-            'url'  => 'admin/settings',
+            'text' => 'Album',
+            'url'  => 'admin/album',
             'icon' => 'fas fa-fw fa-user',
         ],
         [
-            'text' => 'change_password',
-            'url'  => 'admin/settings',
+            'text' => 'Gambar',
+            'url'  => 'admin/gambar',
             'icon' => 'fas fa-fw fa-lock',
         ],
-        [
-            'text'    => 'multilevel',
-            'icon'    => 'fas fa-fw fa-share',
-            'submenu' => [
-                [
-                    'text' => 'level_one',
-                    'url'  => '#',
-                ],
-                [
-                    'text'    => 'level_one',
-                    'url'     => '#',
-                    'submenu' => [
-                        [
-                            'text' => 'level_two',
-                            'url'  => '#',
-                        ],
-                        [
-                            'text'    => 'level_two',
-                            'url'     => '#',
-                            'submenu' => [
-                                [
-                                    'text' => 'level_three',
-                                    'url'  => '#',
-                                ],
-                                [
-                                    'text' => 'level_three',
-                                    'url'  => '#',
-                                ],
-                            ],
-                        ],
-                    ],
-                ],
-                [
-                    'text' => 'level_one',
-                    'url'  => '#',
-                ],
-            ],
-        ],
+
         ['header' => 'Data'],
         [
             'text'       => 'Pengurus',
